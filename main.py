38dc268c21f7cf7c09f5d1c9bb7981151be3b9ed
ninh, christian <cninh@go.pasadena.edu>
2024-07-03 20:30:23 -0700

    Basic skeleton

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+WIDTH = 1632
+HEIGHT = 918
+
+screen = pygame.display_set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Maze Solver')
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    pygame.display.update()
